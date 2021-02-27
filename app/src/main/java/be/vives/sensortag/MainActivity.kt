bb9c9682ef19de4e121518598adcaabb08c1c88f
Hans Naert <hans.naert@vives.be>
2021-02-27 11:20:34 +0100

    add button with onclick through viewbinding

--- app/src/main/java/be/vives/sensortag/MainActivity.kt
+++ app/src/main/java/be/vives/sensortag/MainActivity.kt
@@ -2,10 +2,18 @@ package be.vives.sensortag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import be.vives.sensortag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.scanButton.setOnClickListener { Toast.makeText(this,"Clicked on SCAN button", Toast.LENGTH_LONG).show() }
     }
 }
